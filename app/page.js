6501692b064f378e3931144f350a9da5d64c2a46
AdamFagerberg <Adam.fagerberg1@gmail.com>
2024-08-30 13:57:40 +0200

    map

--- app/page.js
+++ app/page.js
@@ -1,10 +1,28 @@
-"use client"
-import RecipeForm from "./components/RecipeForm"
+"use client";
+import RecipeForm from "./components/RecipeForm";
 import { useEffect, useState } from "react";
 import Recipes from "./mockdata/recipes.json";
+import RecipeCard from "./components/RecipeCard";
 
 export default function Home() {
   const [mockData, setMockData] = useState();
+  const [randomRecipe, setRandomRecipe] = useState();
+
+  useEffect(() => {
+    async function fetchAPI() {
+      try {
+        const response = await fetch(
+          "http://www.themealdb.com/api/json/v1/1/random.php"
+        );
+        const result = await response.json();
+        console.log(result.meals);
+        setRandomRecipe(result.meals);
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+    }
+    fetchAPI();
+  }, []);
 
   useEffect(() => {
     setMockData(Recipes.recipes);
@@ -13,6 +31,35 @@ export default function Home() {
   console.log(mockData);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"> <RecipeForm /></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <RecipeForm />
+      {mockData ? (
+        <div>
+          {mockData.map((recipe) => (
+            <RecipeCard
+              key={recipe.id}
+              title={recipe.title}
+              ingredients={recipe.ingredients}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>Loading..</div>
+      )}
+      {randomRecipe ? (
+        <div>
+          {randomRecipe.map((recipe) => (
+            <RecipeCard
+              key={recipe.idMeal}
+              title={recipe.strMeal}
+              imgSrc={recipe.strMealThumb}
+              ingredients={recipe.strInstructions}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>Loading..</div>
+      )}
+    </main>
   );
 }
