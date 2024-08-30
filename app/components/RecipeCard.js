2b652747ca985ed23674e24734c62deed81df902
Ulfflo <145294663+Ulfflo@users.noreply.github.com>
2024-08-30 11:39:20 +0200

    changes

--- app/components/RecipeCard.js
+++ app/components/RecipeCard.js
@@ -1,8 +1,69 @@
+
+import { useState } from "react";
 import Image from "next/image";
 
-function RecipeCard({ href, imgAlt, imgSrc, children }) {
+function RecipeCard({
+  imgSrc,
+  imgAlt,
+  title,
+  time,
+  ingredients,
+  onEdit,
+  onDelete,
+  onUpdateRating,
+}) {
+  const [clickCount, setClickCount] = useState(0);
+  const [filledStar, setFilledStar] = useState(0);
+
+  const handleThumbsUp = () => {
+    const newClickCount = clickCount + 1;
+    setClickCount(newClickCount);
+    if (newClickCount % 2 === 0 && filledStar < 5) {
+      setFilledStar(filledStar + 1);
+    }
+  };
+
   return (
-    <div className="card rounded-lg w-[360px] mb-10 flex-shrink-0 border shadow-lg overflow-hidden"></div>
+    <div className="card rounded-lg w-[360px] mb-10 flex-shrink-0 border shadow-lg overflow-hidden">
+      <figure>
+        <Image src={imgSrc} alt={imgAlt} width={360} height={200} />
+      </figure>
+      <h2 className="recipeTitle">{title}</h2>
+      <h3 className="cookingTime">{time}</h3>
+      <p className="ingredients">{ingredients}</p>
+      <div className="betyg">
+        <div className="div1">
+          <button className="betygButton" onClick={handleThumbsUp}>
+            <Image
+              id="imgBetyg"
+              src="/img/8530677_thumbs_up_icon.png"
+              alt="betyg"
+              width={24}
+              height={24}
+            />
+          </button>
+          <span id="counter">{clickCount}</span>
+        </div>
+        <div className="div2">
+          {Array.from({ length: 5 }).map((_, i) => (
+            <Image
+              key={i}
+              className="imgstarempty"
+              src={i < filledStar ? "/img/starblack.png" : "/img/starempty.png"}
+              alt="betyg"
+              width={24}
+              height={24}
+            />
+          ))}
+        </div>
+      </div>
+      <button id="editBtn" onClick={onEdit}>
+        Edit
+      </button>
+      <button id="deleteBtn" onClick={onDelete}>
+        Delete
+      </button>
+    </div>
   );
 }
 
---
+++ app/components/RecipeForm.js
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import RecipeCard from "./RecipeCard";
+
+function RecipeForm() {
+  const [recipes, setRecipes] = useState([]);
+  const [newRecipe, setNewRecipe] = useState({
+    title: "",
+    time: "",
+    ingredients: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewRecipe({ ...newRecipe, [name]: value });
+  };
+
+  const handleAddRecipe = (e) => {
+    e.preventDefault();
+    setRecipes([...recipes, { ...newRecipe, id: Date.now() }]);
+    setNewRecipe({ title: "", time: "", ingredients: "" });
+  };
+
+  const handleEditRecipe = (id) => {
+    const recipe = recipes.find((r) => r.id === id);
+    const updatedRecipe = {
+      ...recipe,
+      isEditing: true,
+    };
+    setRecipes(recipes.map((r) => (r.id === id ? updatedRecipe : r)));
+  };
+
+  const handleSaveRecipe = (id, updatedRecipe) => {
+    setRecipes(recipes.map((r) => (r.id === id ? updatedRecipe : r)));
+  };
+
+  const handleDeleteRecipe = (id) => {
+    setRecipes(recipes.filter((r) => r.id !== id));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleAddRecipe} id="recipeForm">
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={newRecipe.title}
+          onChange={handleInputChange}
+          placeholder="Recipe Title"
+          required
+        />
+        <input
+          type="text"
+          id="time"
+          name="time"
+          value={newRecipe.time}
+          onChange={handleInputChange}
+          placeholder="Cooking Time"
+          required
+        />
+        <textarea
+          id="ingredients"
+          name="ingredients"
+          value={newRecipe.ingredients}
+          onChange={handleInputChange}
+          placeholder="Ingredients"
+          required
+        />
+        <button type="submit">Add Recipe</button>
+      </form>
+
+      <div id="recipesContainer">
+        {recipes.map((recipe) =>
+          recipe.isEditing ? (
+            <div key={recipe.id}>
+              <input
+                type="text"
+                value={recipe.title}
+                onChange={(e) =>
+                  handleSaveRecipe(recipe.id, {
+                    ...recipe,
+                    title: e.target.value,
+                    isEditing: false,
+                  })
+                }
+              />
+              <input
+                type="text"
+                value={recipe.time}
+                onChange={(e) =>
+                  handleSaveRecipe(recipe.id, {
+                    ...recipe,
+                    time: e.target.value,
+                    isEditing: false,
+                  })
+                }
+              />
+              <textarea
+                value={recipe.ingredients}
+                onChange={(e) =>
+                  handleSaveRecipe(recipe.id, {
+                    ...recipe,
+                    ingredients: e.target.value,
+                    isEditing: false,
+                  })
+                }
+              />
+              <button
+                onClick={() =>
+                  handleSaveRecipe(recipe.id, { ...recipe, isEditing: false })
+                }
+              >
+                Save
+              </button>
+            </div>
+          ) : (
+            <RecipeCard
+              key={recipe.id}
+              imgSrc="/img/Recept1.png"
+              imgAlt="image of dish"
+              title={recipe.title}
+              time={recipe.time}
+              ingredients={recipe.ingredients}
+              onEdit={() => handleEditRecipe(recipe.id)}
+              onDelete={() => handleDeleteRecipe(recipe.id)}
+            />
+          )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default RecipeForm;
